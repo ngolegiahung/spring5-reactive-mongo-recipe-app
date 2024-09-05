7acb4c63000dbeb7d56fb369922325384e005d5d
Hung L.G. NGO <hung.l.g.ngo@gmail.com>
2024-09-05 22:43:24 +0700

    Init app.

---
+++ src/test/java/guru/springframework/repositories/reactive/UnitOfMeasureReactiveRepositoryTest.java
@@ -0,0 +1,13 @@
+package guru.springframework.repositories.reactive;
+
+import junit.framework.TestCase;
+
+public class UnitOfMeasureReactiveRepositoryTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+}
